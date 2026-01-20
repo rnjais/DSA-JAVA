2118b1ba4b1542d688c0e06f286ef966e6de99f9
Aryan Jaiswal <aj80529754@gmail.com>
2026-01-20 14:14:31 +0000

    commit

--- DSA/SearchingAndSorting/SelectionSort.java
+++ DSA/SearchingAndSorting/SelectionSort.java
@@ -1,7 +1,7 @@
 package DSA.SearchingAndSorting;
-
+import java.util.Scanner;
 public class SelectionSort {
-    public static void method(int a[]) {
+    public static void maxiMumValue(int a[]) {
         int size = a.length; 
         int temp = 0;
         System.out.println("Before Sorting");
@@ -11,14 +11,14 @@ public class SelectionSort {
         System.out.println();
         System.out.println("Sorting");
         for (int i = 0; i < size; i++) {
-            int max = 0;
+            int min = 0;
             for (int j = 1; j < size - i; j++) {
-                if (a[max] < a[j]) {
-                    max = j;
+                if (a[min] < a[j]) {
+                    min = j;
                 }
             }
-            temp = a[max];
-            a[max] = a[size - i - 1];
+            temp = a[min];
+            a[min] = a[size - i - 1];
             a[size - i - 1] = temp;
 
             for (int k : a) {
@@ -32,9 +32,45 @@ public class SelectionSort {
             System.out.print(i + " ");
         }
     }
+    public static void miniMumValue(int a[]) {
+        int size = a.length; 
+        int temp = 0;
+        System.out.println("Before Sorting");
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Sorting");
+        for (int i = 0; i < size; i++) {
+            int min = i;
+            for (int j = i; j < size; j++) {
+                if (a[min] > a[j]) {
+                    min = j;
+                }
+            }
+            temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+
+            for (int k : a) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("After Sorting");
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+    }
     public static void main(String[] args) {
-        int a[] = { 5, 8, 4, 9, 3, 6 };
-        method(a);
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[6];
+        System.out.println("Enter Element Of Array: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        miniMumValue(a);
     }
 
 }

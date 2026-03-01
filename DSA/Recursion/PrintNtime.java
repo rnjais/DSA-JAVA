6370098326ed7df0f4a6e7a358260cacee626d9b
Aryan Jaiswal <aj80529754@gmail.com>
2026-03-01 06:09:43 +0000

    c

--- DSA/Recursion/PrintNtime.java
+++ DSA/Recursion/PrintNtime.java
@@ -1,6 +1,6 @@
 class PrintNtime {
     int n = 0;
-    public static int  re(int n) { 
+    public static int  re(int n) {  //using return
          
         if(n == 5){
             return 5; 
@@ -9,14 +9,21 @@ class PrintNtime {
        return re(n+1);
        
     }
-    static void re(int i, int n){
+    static void re(int i, int n){ // using void
     if(i > n)
     return;
     System.out.println(i);
     re(i+1, n);
     }
+    static void reB(int i, int n){ // using Backtracking
+    if(i == 0)
+    return;
+    reB(i-1, n);
+    System.out.println(i);
+    }
+    
 
     public static void main(String[] args) {
-       re(1,5);
+       reB(5,5);
     }
 }

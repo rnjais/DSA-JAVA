0b14c68ad5d01811a6e5b88c2a8a509ca7c0a326
Aryan Jaiswal <aj80529754@gmail.com>
2026-03-19 06:32:54 +0000

    c

--- LeetCodeProblems/P3MaxConsecutiveOne.java
+++ LeetCodeProblems/P3MaxConsecutiveOne.java
@@ -3,20 +3,21 @@ package LeetCodeProblems;
 public class P3MaxConsecutiveOne {
     static int method(int nums[],int n){
          int count = 0;
+         int max =0;
       for (int i = 0; i < n; i++) {
-       ;
-        if(nums[i]==0){
-            count = 0;
-            continue;
-        }
         if(nums[i]==1){
             count++;
+            if(count > max){
+                max = count;
+            }
+        }else{
+            count = 0;
         }
       }
-      return count;
+      return max;
     }
     public static void main(String[] args) {
-        int nums []= {1,1,0,1,1,1,1,1,1};
+        int nums []= {1,0,1,1,0,1};
         int n = nums.length;
        int consecutive = method(nums, n);
        System.out.println(consecutive);

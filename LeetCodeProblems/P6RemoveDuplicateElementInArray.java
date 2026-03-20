a2f06b4e1c07d56050f94ef10d3da7952e43421b
Aryan Jaiswal <aj80529754@gmail.com>
2026-03-20 17:20:37 +0000

    c

--- LeetCodeProblems/P6RemoveDuplicateElementInArray.java
+++ LeetCodeProblems/P6RemoveDuplicateElementInArray.java
@@ -1,3 +1,5 @@
+
+
 package LeetCodeProblems;
 
 public class P6RemoveDuplicateElementInArray {
@@ -8,15 +10,15 @@ public class P6RemoveDuplicateElementInArray {
     }
       int k = 1;
         for(int i=1; i<nums.length; i++){
-            if(nums[i]!=nums[i-1]){
-                nums[k] = nums[i];
+            if(nums[i]!=nums[i-1]){// Check Element is Unique
+                nums[k] = nums[i];//Stores uniques Element
                 k++;
             }
         }
         return k;
   }
     public static void main(String args[]){
-         int[] nums = {0,0,1,1,1,2,2,3,3,4};
+         int[] nums = {1,2,2,3};
 
         int k = a1(nums);
 
@@ -30,4 +32,5 @@ public class P6RemoveDuplicateElementInArray {
    
    
    }
+
 

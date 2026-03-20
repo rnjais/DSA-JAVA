f535d807634cc8858f764d1e8e513a1e0cdda3d9
Aryan Jaiswal <aj80529754@gmail.com>
2026-03-20 07:54:10 +0000

    c

--- LeetCodeProblems/P4CheckArrayIsSorted.java
+++ LeetCodeProblems/P4CheckArrayIsSorted.java
@@ -1,25 +1,34 @@
 package LeetCodeProblems;
 
 public class P4CheckArrayIsSorted {
-    
+
     // Function to check if the array is sorted
     public static boolean isSorted(int[] arr, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 // If any element is smaller than the previous one, return false
-                if (arr[j] < arr[i]) 
+                if (arr[j] < arr[i])
                     return false;
             }
         }
         return true; // Return true if no unsorted elements are found
     }
+
+    public static boolean isSorted1(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i - 1] > arr[i])
+                return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = { 1, 2, 3, 4, 5 };
         int n = 5;
-       boolean ans =isSorted(arr, n);
+        boolean ans = isSorted1(arr, n);
         // Output result
-       System.out.println(ans);
+        System.out.println(ans);
     }
 }
-
-

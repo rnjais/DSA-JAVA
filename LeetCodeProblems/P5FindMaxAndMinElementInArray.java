ad411c183dee1f5ffcf1bf885216a3ba487cf234
Aryan Jaiswal <aj80529754@gmail.com>
2026-03-20 15:44:10 +0000

    c

--- LeetCodeProblems/P5FindMaxAndMinElementInArray.java
+++ LeetCodeProblems/P5FindMaxAndMinElementInArray.java
@@ -1,5 +1,21 @@
 package LeetCodeProblems;
 
 public class P5FindMaxAndMinElementInArray {
-    
+    static String a1(int arr[]){
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(max < arr[i]){
+                max = arr[i];
+            }
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        return "Maximum Element: "+max+"\nMinimum Element: "+ min;
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,1,5,10};
+        System.out.println(a1(arr));
+    }
 }

704b1f9655f03ed2bdb2859cc33df99a76a4ad4b
Aryan Jaiswal <aj80529754@gmail.com>
2026-03-21 09:10:53 +0000

    c

--- LeetCodeProblems/P8CheckInsertPosition.java
+++ LeetCodeProblems/P8CheckInsertPosition.java
@@ -1,27 +1,15 @@
 package LeetCodeProblems;
 
 public class P8CheckInsertPosition {
-    public static int searchInsert(int[] nums, int target) {
-        int targetIndex = 0;
-        int i = 0;
-        for (i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                targetIndex = i;
-            }
+   public static int searchInsert(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
         }
-        i++;
-        if (i == nums.length) {
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[j - 1] <= target && target <= nums[j]) {
-                    targetIndex = j;
-                }
-            }
-        }
-        return targetIndex;
     }
-
-    public static void main(String[] args) {
-        int nums[] = { 1, 3, 5, 6 };
+    return nums.length;
+}    public static void main(String[] args) {
+        int nums[] = {1,3,5,6};
         int target = 4;
         System.out.println(searchInsert(nums, target));
     }
